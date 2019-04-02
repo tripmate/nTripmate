529b8b3418275d22083c427db5a8aadf78d6a01f
lbc <dlqudcjf55@gmail.com>
2019-04-02 20:33:37 +0900

    add created project

---
+++ src/main/java/kr/co/tripmate/TripmateApplication.java
@@ -0,0 +1,13 @@
+package kr.co.tripmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/tripmate/TripmateApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.co.tripmate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TripmateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
