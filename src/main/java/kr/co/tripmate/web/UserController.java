a8de37d945e5e110ddb8d2f6eb78f1bed345a160
lbc <dlqudcjf55@gmail.com>
2019-04-07 16:42:42 +0900

    로그인 기능, 로그인 여부에 따른 메뉴 표시 기능 추가

--- src/main/java/kr/co/tripmate/web/UserController.java
+++ src/main/java/kr/co/tripmate/web/UserController.java
@@ -1,5 +1,9 @@
 package kr.co.tripmate.web;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +24,44 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@GetMapping(value = "/loginForm")
+	public String loginForm() {
+		
+		return "/user/login";
+		
+	}
+	
+	@PostMapping(value = "/login")
+	public String login(String userId, String password, HttpSession session) {
+		
+		User user = userRepository.findByUserId(userId);
+		
+		if ( user == null ) {
+			System.out.println("Login Failure!!");
+			return "redirect:/users/loginForm";
+		}
+		
+		if ( !password.equals(user.getPassword()) ) {
+			System.out.println("Login Failure!!");
+			return "redirect:/users/loginForm";
+		}
+		
+		System.out.println("Login Success!!");
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
+		
+	}
+	
+	@GetMapping(value = "/logout")
+	public String logout(HttpSession session) {
+		
+		session.removeAttribute("user");
+		
+		return "redirect:/";
+		
+	}
 	
 	@GetMapping(value = "/form")
 	public String form() {
