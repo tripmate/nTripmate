dbe53af84083b443b088a84d49a02460527708c2
LEEBC <bc3411@naver.com>
2020-12-20 20:14:26 +0900

    ddddddddddds3

--- src/main/java/kr/co/tripmate/TripmateApplication.java
+++ src/main/java/kr/co/tripmate/TripmateApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TripmateApplication {
-	// asdf
+	// asdfdfdf
 	public static void main(String[] args) {
 		SpringApplication.run(TripmateApplication.class, args);
 	}
